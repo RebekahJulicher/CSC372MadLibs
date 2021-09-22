a4122f86c474204ff735f463c8ec1e2252c8b0c3
Orlando Rodriguez <orlandorodriguezosr@protonmail.ch>
2021-09-21 20:58:25 -0700

    First commit

---
+++ CSC372MadLibs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC372MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
