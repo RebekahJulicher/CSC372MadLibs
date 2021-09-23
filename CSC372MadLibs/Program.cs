32a4fae5f64eb107a383e6df149e515f01e7ece9
Orlando Rodriguez <orlandorodriguezosr@protonmail.ch>
2021-09-23 09:22:12 -0700

    Going to finish up some methods and commit

--- CSC372MadLibs/Program.cs
+++ CSC372MadLibs/Program.cs
@@ -5,7 +5,7 @@ using CSC372MadLibs.Template;
 namespace CSC372MadLibs {
     class Program {
         static string [] partsOfSpeech = new string {"noun", "plnoun", "verb", "adjective", "integer"};
-        static void Main(string[] args){
+        static void Main(string[] args) {
             string templateString = extractTemplate("templates.txt");
             var template = new Template(templateString, extractPOS(templateString));
             Console.WriteLine("Hello World!");
@@ -24,6 +24,10 @@ namespace CSC372MadLibs {
             foreach (int part in parts) 
                 partString += partsOfSpeech[part - 1] + " ";
             return partString.Split(' ');
+        }
+
+        static void askForPOS() {
+
         }
     }
 }
