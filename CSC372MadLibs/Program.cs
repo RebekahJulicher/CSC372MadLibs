d0ca5b1a9403df637e47c508acea1f86c8a2a4dc
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2021-09-26 13:59:43 -0700

    Template sorted, needs loop
    Not sure quite what all of the extra files are, it just stuck those in

--- CSC372MadLibs/Program.cs
+++ CSC372MadLibs/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using CSC372MadLibs.Template;
+using CSC372MadLibs.Templates;
 
 namespace CSC372MadLibs {
     class Program {
-        static string [] partsOfSpeech = new string {"noun", "plnoun", "verb", "adjective", "integer"};
+        static string[] partsOfSpeech = {"noun", "plnoun", "verb", "adjective", "integer"};
 
         static void Main(string[] args) {
             string templateString = extractTemplate("templates.txt");
-            var template = new Template(templateString, extractPOS(templateString));
-            Console.WriteLine("Hello World!");
+            var template = new Template(templateString);
+            Console.WriteLine("Hello World from MadLibs!");
         }
 
+        // Reads all lines of a template file into a string array, and 
+        // returns a random template line
         static string extractTemplate (string fileName) {
             string [] lines = System.IO.File.ReadAllLines(fileName);
             Random rand = new Random();
@@ -20,8 +22,13 @@ namespace CSC372MadLibs {
             return lines[templateIndex];
         }
 
+        // Locates the parts of speech in the string (categorized by int) and 
+        // puts the part of speech to prompt for into a string
+        /*
         static string [] extractPOS(string template) {
             string partString = string.Empty;
+
+            // This only works on collections, why turn it into an array after using Where?
             int [] parts = template.Where(Char.IsDigit).ToArray());
             foreach (int part in parts) 
                 partString += partsOfSpeech[part - 1] + " ";
@@ -36,8 +43,7 @@ namespace CSC372MadLibs {
                 string input = Console.ReadLine();
                 POS.Add(key, input);
             }
-                
-
         }
+        */
     }
 }
--- CSC372MadLibs/Template.cs
+++ CSC372MadLibs/Template.cs
@@ -1,24 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using CSC372MadLibs.Program;
+using static CSC372MadLibs.Program;
 
-namespace CSC372MadLibs.Template {
+namespace CSC372MadLibs.Templates {
     public class Template {
         private string fileStream = string.Empty;
-        private Dictionary<string, List<string>> POSMappings = new Dictionary<string, string>();
-        private string filledTemplate = string.Empty;
+        //private Dictionary<string, List<string>> POSMappings = new Dictionary<string, string>();
+        //private string filledTemplate = string.Empty;
+        private string[] template;
+        private int currPart = 0;
 
-        public Template(string fileStream, string [] partsOfSpeech) {
+        // Constructor, reads in the filestream for the template and sets
+        // each of the parts of speech to new Lists in the POSMapping Dictionary
+        public Template(string fileStream) {
             this.fileStream = fileStream;
-            foreach (string part in partsOfSpeech)
-                POSMappings.Add(part, new List<string> { });
+            template = fileStream.Split(' ');
         }
 
-        public string GetFileStream() {
-            return fileStream;
+        // removed GetFileStream for now because it's covered in ToString
+
+        // Gets the next part of speech after the current index
+        public string getNextPart(){
+            if (currPart > template.Length - 1 ) return null;
+            bool found = false;
+            for (int i = currPart; i < template.Length && !found; i++){
+                if (Char.IsDigit(template[i][0])){
+                    found = true;
+                    currPart = i;
+                    return template[i];
+                }
+            }
+            currPart = template.Length;
+            return null;
+        }
+
+        // Sets the current part of speech to the response
+        public void setCurrPart(string response){
+            template[currPart] = response;
+            currPart++;
         }
         
-        public Dictionary<string, Lis<string>> GetPOSMappings() {
+        /*
+        public Dictionary<string, List<string>> GetPOSMappings() {
             return POSMappings;
         }
 
@@ -30,9 +53,11 @@ namespace CSC372MadLibs.Template {
         public void fillTemplate() {
             for ()
         }
+        */
 
         public override string ToString() {
-            return fileStream;
+            if (currPart >= template.Length) return fileStream;
+            return String.Join(" ", template);
         }
     }
 }
