591d4dd5979cc0119554baf44a1ef468ced4e572
Orlando Rodriguez <orlandorodriguezosr@protonmail.ch>
2021-09-27 23:21:19 -0700

    Fixed weird looping

--- CSC372MadLibs/Program.cs
+++ CSC372MadLibs/Program.cs
@@ -5,32 +5,65 @@ using CSC372MadLibs.Templates;
 
 namespace CSC372MadLibs {
     class Program {
-        //static string[] partsOfSpeech = {"noun", "plnoun", "verb", "adjective", "integer"};
+        static string[] validInputs = {"y", "yes", "n", "no"};
 
         static void Main(string[] args) {
-            string templateString = extractTemplate("templates.txt");
-            var template = new Template(templateString);
-            string playing = "y";
+            bool running = true;
 
-            while (String.Equals(playing, "y")){
-                Console.WriteLine("Welcome to Mad Libs! Would you like to play? y/n: ");
+            string playing = "0";
+            bool valid = false;
+
+            // Asks user if they want to play for the first time
+            while (!valid) {
+                Console.WriteLine("\nWelcome to Mad Libs! Would you like to play? y/n: ");
                 playing = Console.ReadLine();
-                while (!String.Equals(playing, "y") && !String.Equals(playing, "n")){
-                    Console.WriteLine("Invalid input! Would you like to play? y/n: ");
-                    playing = Console.ReadLine();
-                }
-                // If we're still playing, play a round
-                if (String.Equals(playing, "y")){
-                    string curr = template.getNextPart();
-                    Console.WriteLine(curr);
-                    while (!String.Equals(curr, null)){
-                        Console.WriteLine("Enter a " + curr + ": ");
-                        template.setCurrPart(Console.ReadLine());
-                        curr = template.getNextPart();
-                    }
-                    Console.WriteLine(template.ToString());
-                }
+                playing = playing.ToLower();
+                if (validInputs.Contains(playing))
+                    valid = true;
+                else 
+                    Console.WriteLine("Invalid input");
             }
+
+            // Sets the running boolean to true or false
+            if (playing[0] == 'y') 
+                running = true;
+            else
+                running = false;
+
+            // Executes as long as the player wants to play
+            while (running) {
+                Console.WriteLine("\n");
+                // Actually playing the game
+                string templateString = extractTemplate("templates.txt");
+                var template = new Template(templateString);
+                string curr = template.getNextPart();
+                //Console.WriteLine(curr);
+                while (!String.Equals(curr, null)){
+                    Console.WriteLine("Enter a " + curr + ": ");
+                    template.setCurrPart(Console.ReadLine());
+                    curr = template.getNextPart();
+                }
+                Console.WriteLine(template.ToString());
+
+                valid = false;
+                // Asks user if they want to play again
+                while (!valid) {
+                    Console.WriteLine("\nWant to play again?: y/n"); // Change this to something non-degenerate
+                    playing = Console.ReadLine();
+                    playing = playing.ToLower();
+                    if (validInputs.Contains(playing))
+                        valid = true;
+                    else 
+                        Console.WriteLine("Invalid input");
+                }
+
+                // sets the running boolean to true or false
+                if (playing[0] == 'y') 
+                    running = true;
+                else 
+                    running = false;
+            }
+            Console.WriteLine("Ok byeeeee :D");
         }
 
         // Reads all lines of a template file into a string array, and 
@@ -41,29 +74,5 @@ namespace CSC372MadLibs {
             int templateIndex = rand.Next(lines.Length);
             return lines[templateIndex];
         }
-
-        // Locates the parts of speech in the string (categorized by int) and 
-        // puts the part of speech to prompt for into a string
-        /*
-        static string [] extractPOS(string template) {
-            string partString = string.Empty;
-
-            // This only works on collections, why turn it into an array after using Where?
-            int [] parts = template.Where(Char.IsDigit).ToArray());
-            foreach (int part in parts) 
-                partString += partsOfSpeech[part - 1] + " ";
-            return partString.Split(' ');
-        }
-
-        static void askForPOS(Template template) {
-            Dictionary<string, string> POS = template.getPOSMappings();
-            var keys = POS.Keys.ToList();
-            foreach (var key in keys) {
-                Console.WriteLine("Enter a {0}: ", key);
-                string input = Console.ReadLine();
-                POS.Add(key, input);
-            }
-        }
-        */
     }
 }
